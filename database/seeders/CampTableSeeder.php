c2b09d5fb9f62fabb70d93749eac8bcfda2de0c1
dimas <dimaspradipto@gmail.com>
2023-09-07 22:00:43 +0700

    build: seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                "title" => "Gila Belajar",
+                "slug" => "gila-belajar",
+                "price" =>280,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "title" => "Baru Mulai",
+                "slug" => "baru-mulai",
+                "price" =>140,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ]
+        ];
+
+        foreach ($camps as $camp) {
+            Camp::create($camp);
+        }
+    }
+}
